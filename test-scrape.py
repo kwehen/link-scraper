3ffbb1ad86593e65a08b4ccc14423ce70a847c03
kwehen <kwesihenry11@gmail.com>
2023-10-08 16:00:44 -0400

    removed uneeded code and added comments

--- test-scrape.py
+++ test-scrape.py
@@ -6,29 +6,33 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 import sensitive
 
+# Web Driver for Firefox
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
+# Get Linkedin login page
 driver.get("https://linkedin.com/uas/login")
 time.sleep(5)
 
+# Login
 username = driver.find_element(By.ID, "username")
 username.send_keys(sensitive.username)
 password = driver.find_element(By.ID, "password")
 password.send_keys(sensitive.password)
-
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
+# Wait for page to load
 time.sleep(3)
 
+# Go to the jobs page
 jobs_link = driver.find_element(By.LINK_TEXT, "Jobs")
 jobs_link.click()
 
+# Wait for page to load
 time.sleep(3)
 
-job_src = driver.page_source
-soup = BeautifulSoup(job_src, 'lxml')
-
+# Search for specific job title
 search = driver.find_element(By.CLASS_NAME, "jobs-search-box__text-input")
+# In the future, change this to input variable so that a user can scrape for their specified job title
 search.send_keys("Devops Engineer")
 time.sleep(1)
 search.send_keys(u'\ue007')
